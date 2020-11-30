44daf7f20b58ddc0b57f706e2841ef84c501e93b
Melissa Hazlewood <meli.hazlewood@gmail.com>
2020-11-29 18:26:25 -0800

    Update backprop.py

--- src/backprop.py
+++ src/backprop.py
@@ -1,48 +1,38 @@
 import numpy as np
 
-ETA = 0.1 #learning rate
-
-def sigmoid(x):
-	return 1.0/(1 - np.exp(-x))
-
-def sigmoid_deriv(x):
-	return sigmoid(X) * (1 - sigmoid(x))
-
-
-def backpropagation(hidden_layer, output_layer, attribute_vector):
+def backpropagation(hidden_layer, output_layer, output_vector, target_vector, hidden_vector, attribute_vector, eta=0.1):
 	# TODO: WRONG... FIX
 	hidden_layer = np.array(hidden_layer)
 	output_layer = np.array(output_layer)
+	output_vector = np.array(output_vector)
+	target_vector = np.array(target_vector)
+	hidden_vector = np.array(hidden_vector)
 	attribute_vector = np.array(attribute_vector)
 
-	for output_node in output_layer:
-		output_responsibility = np.multiply(np.multiply(output_layer, (1 - output_layer)), attribute_vector)
-	for hidden_node in hidden_layer:
-		hidden_responsibility = np.multiply(np.multiply(hidden_layer, (1 - hidden_layer)), output_responsibility.dot(output_layer))
+	output_responsibility = np.multiply(np.multiply(output_vector, (1 - output_vector)), (target_vector - output_vector))
+	hidden_responsibility = np.multiply(np.multiply(hidden_vector, (1 - hidden_vector)), output_responsibility.dot(output_layer))
+	print("output_responsibility.dot(output_layer):", output_responsibility.dot(output_layer))
 
-	output_layer = output_layer + ETA*np.multiply(output_responsibility, hidden_layer)
-	hidden_layer = hidden_layer + ETA*np.multiply(hidden_responsibility, attribute_vector)
+	output_layer = output_layer + eta*np.multiply(output_responsibility, hidden_vector)
+	hidden_layer = hidden_layer + eta*np.multiply(hidden_responsibility, attribute_vector)
 
 	return hidden_layer, output_layer
 
 if __name__ == "__main__":
-	# TODO: WRONG... check example
-	# example copied from dennis lol
-    # example from book, table 5.1. slighty off due to rounding i think, but it shouldn't matter
-    hidden_layer = [
-        [-1.0, 0.5],
-        [0.1, 0.7]
-    ]
+	# Example from Table 5.3 in Kubat
+	hidden_layer = [ [-1.0, 1.0],
+					 [1.0, 1.0] ]
 
-    output_layer = [
-        [0.9, 0.5],
-        [-0.3, -0.1]
-    ]
+	output_layer = [ [1.0, 1.0],
+	  				 [1.0, -1.0] ]
 
-    attribute_vector = [0.8, 0.1]
+	output_vector = [0.65, 0.59]
+	target_vector = [1.0, 0.0]
+	hidden_vector = [0.12, 0.5]
+	attribute_vector = [1.0, -1.0]
 
-    forward_prop_results = backpropagation(hidden_layer, output_layer, attribute_vector)
-    # hidden node outputs
-    print(forward_prop_results[0])
-    # output node outputs
-    print(forward_prop_results[1])
+	backprop_results = backpropagation(hidden_layer, output_layer, output_vector, target_vector, hidden_vector, attribute_vector)
+	# Hidden node outputs/weights
+	print(backprop_results[0])
+	# Output node outputs/weights
+	print(backprop_results[1])
