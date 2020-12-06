8a5849347d0bf56209b3b23c259e764139fe5198
rlherbert <rlherbert.dev@gmail.com>
2020-12-06 11:48:12 -0800

    old

--- src/mlp.py
+++ src/mlp.py
@@ -1,20 +1,22 @@
 import math
 
+
 def __sigmoid(swixi):
-        return 1/(1 + math.pow(math.e, -swixi))
+    return 1/(1 + math.pow(math.e, -swixi))
+
 
 class mlp:
     def __init__(self):
         pass
 
-    def train():
+    def train(self):
         pass
 
-    def get_classification():
+    def get_classification(self):
         pass
 
-    def print_weights():
-        #or output to file?? idk what to call this one
+    def print_weights(self):
+        # or output to file?? idk what to call this one
         pass
 
     def _forward_prop(hidden_layer, output_layer, attribute_vector):
@@ -53,11 +55,17 @@ class mlp:
         num_hid = len(hidden_vector)
         num_out = len(output_vector)
 
-        output_responsibility = np.multiply(np.multiply(output_vector, (1 - output_vector)), (target_vector - output_vector))
-        hidden_responsibility = np.multiply(np.multiply(hidden_vector, (1 - hidden_vector)), output_responsibility.dot(output_layer))
+        output_responsibility = np.multiply(np.multiply(
+            output_vector, (1 - output_vector)), (target_vector - output_vector))
+        hidden_responsibility = np.multiply(np.multiply(
+            hidden_vector, (1 - hidden_vector)), output_responsibility.dot(output_layer))
 
-        output_layer = output_layer + eta*np.multiply(np.array([output_responsibility,]*num_hid).transpose(), hidden_vector)
-        hidden_layer = hidden_layer + eta*np.multiply(np.array([hidden_responsibility,]*num_out).transpose(), attribute_vector)
+        output_layer = output_layer + eta * \
+            np.multiply(np.array([output_responsibility, ]
+                                 * num_hid).transpose(), hidden_vector)
+        hidden_layer = hidden_layer + eta * \
+            np.multiply(np.array([hidden_responsibility, ]
+                                 * num_out).transpose(), attribute_vector)
 
         return hidden_layer, output_layer
 
@@ -77,26 +85,28 @@ if __name__ == "__main__":
 
     attribute_vector = [0.8, 0.1]
 
-    forward_prop_results = _forward_prop(hidden_layer, output_layer, attribute_vector)
+    forward_prop_results = _forward_prop(
+        hidden_layer, output_layer, attribute_vector)
     # Hidden node outputs
     print(forward_prop_results[0])
     # Output node outputs
     print(forward_prop_results[1])
-    
+
     # Testing backpropagation
     # Example from Table 5.3 in Kubat
-    hidden_layer = [ [-1.0, 1.0],
-                     [1.0, 1.0] ]
+    hidden_layer = [[-1.0, 1.0],
+                    [1.0, 1.0]]
 
-    output_layer = [ [1.0, 1.0],
-                     [-1.0, 1.0] ]
+    output_layer = [[1.0, 1.0],
+                    [-1.0, 1.0]]
 
     output_vector = [0.65, 0.59]
     target_vector = [1.0, 0.0]
     hidden_vector = [0.12, 0.5]
     attribute_vector = [1.0, -1.0]
 
-    backprop_results = backpropagation(hidden_layer, output_layer, output_vector, target_vector, hidden_vector, attribute_vector)
+    backprop_results = backpropagation(
+        hidden_layer, output_layer, output_vector, target_vector, hidden_vector, attribute_vector)
     # Hidden node outputs/weights
     print(backprop_results[0])
     # Output node outputs/weights
